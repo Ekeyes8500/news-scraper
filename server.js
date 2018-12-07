76bf5ac798cfc8229912c71e4006c8c0265f9bae
Eric Keyes <keyes.eric@outlook.comgit config --global user.email keyes.eric@outlook.com>
2018-12-07 11:00:47 -0500

    initial setup, directories labeled and ready for deployment

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var app = express();
+var db = require("./models");
+
+
+// Configure middleware
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/newsScraperDB", { useNewUrlParser: true });
+
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
+
+module.exports = app;
